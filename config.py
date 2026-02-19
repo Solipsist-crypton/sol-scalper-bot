54dc7ebdae29d3a96f956bf9d891a7c9ce914868
Solipsist-crypton <solipsist-crypto@ukr.net>
2026-02-20 00:08:01 +0200

    Fix close_position for Binance API

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
 import os
 
 # ===== НАЛАШТУВАННЯ =====
-# Telegram (буде в змінних Railway)
+# Telegram
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
-CHANNEL_ID = -1003877678504  # Додайте цей рядок
+CHANNEL_ID = -1003877678504  # ID каналу
+
+# 🟢 ДОДАЙТЕ ЦІ РЯДКИ - Binance API ключі
+EXCHANGE_API_KEY = os.getenv('EXCHANGE_API_KEY')
+EXCHANGE_API_SECRET = os.getenv('EXCHANGE_API_SECRET')
 
 # Торгові налаштування
-#SYMBOLS = ['SOLUSDT', 'BTCUSDT', 'ETHUSDT', 'ARBUSDT', 'LINKUSDT', 'AVAXUSDT',  'DOTUSDT', 'UNIUSDT', 'APTUSDT']
-SYMBOLS = ['BTCUSDT']
+SYMBOLS = ['SOLUSDT', 'BTCUSDT', 'ETHUSDT', 'ARBUSDT', 'LINKUSDT', 'AVAXUSDT', 'DOTUSDT', 'UNIUSDT', 'APTUSDT']
 INTERVAL = '5m'
 EMA_FAST = 20
 EMA_SLOW = 50
